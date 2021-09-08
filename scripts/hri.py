f498000799b588d39805b3bfc02ab7d2e4ad4ae3
omotoye <adekoyaomotoye@gmail.com>
2021-09-08 12:01:15 +0100

    Updated the hri node

--- scripts/hri.py
+++ scripts/hri.py
@@ -3,6 +3,7 @@
 # import ROS headers
 import rospy
 import actionlib
+from std_msgs import String
 
 # import PAL Robotics custom headers
 from pal_interaction_msgs.msg import TtsAction, TtsGoal
@@ -14,6 +15,10 @@ from sciroc_hri.msg import HRIAction, HRIFeedback, HRIResult
 import os
 import dialogflow
 from google.api_core.exceptions import InvalidArgument
+from dialogflow_ros import DialogflowClient
+from dialogflow_ros.msg import *
+
+import time
 
 
 class HRI:
@@ -22,20 +27,10 @@ class HRI:
         self._feedback = HRIFeedback()
         self._result = HRIResult()
         self._action_name = name
-        self.text_to_be_analysed = ""
-
-        # setting up dialogflow
-        os.environ[
-            "GOOGLE_APPLICATION_CREDENTIALS"
-        ] = "gentle-proton-252714-066b7ef02309.json"
-
-        self.DIALOGFLOW_PROJECT_ID = "gentle-proton-252714"
-        self.DIALOGFLOW_LANGUAGE_CODE = "en"
-        self.SESSION_ID = "sciroc_hri"
-        self.session_client = dialogflow.SessionsClient()
-        self.session = self.session_client.session_path(
-            self.DIALOGFLOW_PROJECT_ID, self.SESSION_ID
-        )
+        rospy.Subscriber("/dialogflow_text", String, self.transcript_cb)
+        self.dc = DialogflowClient()
+        self.dr = DialogflowRequest()
+        self.transcript = " "
 
         # Initialize the hri_action_server to listen for goal from client
         self._as = actionlib.SimpleActionServer(
@@ -48,77 +43,95 @@ class HRI:
 
         self.text = ""
 
-    def call_dialogflow(self):
-        text_input = dialogflow.types.TextInput(
-            text=self.text_to_be_analysed, language_code=self.DIALOGFLOW_LANGUAGE_CODE
-        )
-        query_input = dialogflow.types.QueryInput(text=text_input)
-        try:
-            response = self.session_client.detect_intent(
-                session=self.session, query_input=query_input
-            )
-        except InvalidArgument:
-            raise
-        return response
+    def transcript_cb(self, msg):
+        self.transcript = msg
 
-    def tts_event(self, event):
-        return {
-            1: "TTS_EVENT_INITIALIZATION",
-            2: "TTS_EVENT_SHUTDOWN",
-            4: "TTS_EVENT_SYNCHRONIZATION",
-            8: "TTS_EVENT_FINISHED_PLAYING_UTTERANCE",
-            16: "TTS_EVENT_MARK",
-            32: "TTS_EVENT_STARTED_PLAYING_WORD",
-            64: "TTS_EVENT_FINISHED_PLAYING_PHRASE",
-            128: "TTS_EVENT_FINISHED_PLAYING_SENTENCE",
-        }[event]
-
-    def tts_feedbackCb(self, feedback):
-        print("event type: " + self._tts_event(feedback.event_type))
-        print("timestamp: " + str(feedback.timestamp))
-        print("current word: " + feedback.text_said)
-        print("next word: " + feedback.next_word)
-        print("-")
+    def call_dialogflow(self, text):
+        self.dr.query_text = text
+        ## TODO there should be some sort of while loop here to
+        # keep taking a response from dialogflow and sending the
+        # text from the customer until something happens.
+        resp = self.dc.detect_intent_text(self.dr)
+        self.text = resp.fulfillment_text
+        self.say_something()
+        self.dr.query_text = self.transcript
+        resp = self.dc.detect_intent_text(self.dr)
 
     def say_something(self):
-        client = actionlib.SimpleActionClient("tts_to_soundplay", TtsAction)
+        client = actionlib.SimpleActionClient("tts/goal", TtsAction)
         rospy.loginfo("Waiting for Server")
         client.wait_for_server()
         rospy.loginfo("Reached Server")
         goal = TtsGoal()
         goal.rawtext.text = self.text
         goal.rawtext.lang_id = "en_GB"
-        client.send_goal(goal, feedback_cb=self.tts_feedbackCb)
+        client.send_goal(goal)
         client.wait_for_result()
         res = client.get_result()
-        print("text: " + res.text)
-        print("warning/error msgs: " + res.msg)
-        print("---")
-        return True
+
+    # def tts_event(self, event):
+    #     return {
+    #         1: "TTS_EVENT_INITIALIZATION",
+    #         2: "TTS_EVENT_SHUTDOWN",
+    #         4: "TTS_EVENT_SYNCHRONIZATION",
+    #         8: "TTS_EVENT_FINISHED_PLAYING_UTTERANCE",
+    #         16: "TTS_EVENT_MARK",
+    #         32: "TTS_EVENT_STARTED_PLAYING_WORD",
+    #         64: "TTS_EVENT_FINISHED_PLAYING_PHRASE",
+    #         128: "TTS_EVENT_FINISHED_PLAYING_SENTENCE",
+    #     }[event]
+
+    # def tts_feedbackCb(self, feedback):
+    #     print("event type: " + self._tts_event(feedback.event_type))
+    #     print("timestamp: " + str(feedback.timestamp))
+    #     print("current word: " + feedback.text_said)
+    #     print("next word: " + feedback.next_word)
+    #     print("-")
+
+    # def say_something(self):
+    #     client = actionlib.SimpleActionClient("tts_to_soundplay", TtsAction)
+    #     rospy.loginfo("Waiting for Server")
+    #     client.wait_for_server()
+    #     rospy.loginfo("Reached Server")
+    #     goal = TtsGoal()
+    #     goal.rawtext.text = self.text
+    #     goal.rawtext.lang_id = "en_GB"
+    #     client.send_goal(goal, feedback_cb=self.tts_feedbackCb)
+    #     client.wait_for_result()
+    #     res = client.get_result()
+    #     print("text: " + res.text)
+    #     print("warning/error msgs: " + res.msg)
+    #     print("---")
+    #     return True
 
     def execute_cb(self, goal):
         if goal.mode == 0:
             # Announce Text
             self.text = goal.text
-            self._result.result = self.say_something()
+            self.say_something()
 
         elif goal.mode == 1:
             self.text_to_be_analysed = "Take Order"
             response = self.call_dialogflow()
+            # TODO this is the only place dialogflow is needed
             # Take Order
-            pass
 
         elif goal.mode == 2:
             # Greet
-            pass
+            self.text = "Hello there!,  How are you today?"
+            self.say_something()
 
         elif goal.mode == 3:
             # Take Item
-            pass
+            self.text = "Please can you put the items on my tray!,  i will wait for fifteen seconds and then i leave to delevery the order "
+            self.say_something()
+            time.sleep(15)
 
         elif goal.mode == 4:
+            self.text = "Please take the drinks that you've order from  my tray,  I will wait for fifteen seconds for you to do that"
+            self.say_something()
+            time.sleep(15)
             # Drop Item
-            pass
 
         rospy.loginfo("%s: Succeeded" % self._action_name)
         self._as.set_succeeded(self._result)
